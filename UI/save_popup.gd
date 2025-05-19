b5e020ce9c66b3075c0aa4e08d90b4c68f12cc5c
cparadis777 <c.paradis77@outlook.com>
2025-05-19 08:00:35 -0400

    Added save shortcut to popup

--- UI/save_popup.gd
+++ UI/save_popup.gd
@@ -6,6 +6,11 @@ signal create_mission(name: String)
 func close() -> void:
 	self.queue_free()
 
+func _process(_delta: float) -> void:
+	if not %Name.text.is_empty() and Input.is_action_just_pressed("ui_accept"):
+		self.save()
+
+
 func save() -> void:
 	self.create_mission.emit(%Name.text)
 	self.hide()
